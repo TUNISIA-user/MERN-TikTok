0b83a6b1d68b45d0eadb3c28e0d4aa6997c1376c
Ghaith Nahdi <121437369+TUNISIA-user@users.noreply.github.com>
2024-08-23 13:27:11 +0100

    Update server.js
    change__server__setting

--- TikTok-clone-Back-End/server.js
+++ TikTok-clone-Back-End/server.js
@@ -1,11 +1,11 @@
 import express from "express"
 import mongoose from "mongoose"
- 
 import Videos from "./dbModel.js"
- 
 import cors from "cors"
 
  
+
+
 
 
  
@@ -14,7 +14,7 @@ import cors from "cors"
 const app = express()
 const Port =5000
  
-
+//  connection with data bases 
 const connection_url = 'mongodb+srv://admin:rZ3ai6sNuXDZVaAu@cluster0.kuql1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 mongoose.connect(connection_url, {
@@ -28,25 +28,27 @@ mongoose.connect(connection_url, {
 
 
 app.use(cors({
-    origin: "http://localhost:3000", // Adjust as needed
+    origin: "http://localhost:3000",                  // this to acces the data from api 
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"]
 }));
-app.use(express.json({ limit: '1000mb' })); // Increase size limit
+
+app.use(express.json({ limit: '1000mb' }));                             // Increase size limit
 app.use(express.urlencoded({ limit: '1000mb', extended: true }));
 app.use(express.json())
-//db conifg
+ 
 
 
 //api endpoints
  
-app.get("/",(req,res)=>res.status(200).send(`mongo db  ${Port }...`))
+app.get("/",(req,res)=>res.status(200).send(`mongo db  ${Port }...`)) // the for check if mongo working 
 
+// Posts 1 to data bases   Fetch
 app.get("/v1/posts",async(req,res)=>{
     try{
       const DataFromBaseDeDonne = await Videos.find()
       
-      res.status(200).send(DataFromBaseDeDonne)
+      res.status(200).send(DataFromBaseDeDonne)     // this send request to data base to ensure this data base ok by 200
     }
     catch(err){
         console.log(`thi eroor in ${err}`)
@@ -57,25 +59,33 @@ app.get("/v1/posts",async(req,res)=>{
 
  
 /** 
-  @desc  register user 
+  @desc  register user  to data bases 
 */
 app.post("/v2/posts", async (req, res) => {
     // Post request to add a video document to the videos collection
     const dbVideos = req.body;
 
     try {
-        const data = await Videos.create(dbVideos);
-        res.status(201).send(data);
+        const data = await Videos.create(dbVideos);    // this for store in this data bases 
+        res.status(201).send(data); // ensure send reponse  200 in status 
     } catch (err) {
         res.status(500).send(err);
     }   
 });
 
+
+ 
+/** 
+  @desc  find item from  the databases
+*/
+
+
+
 app.get("/v2/posts", async (req, res) => {  
     // Post request to add a video document to the videos collection
          try{
-                const data =await Videos.find()
-                res.status(200).send(data)
+                const data =await Videos.find()   // find from the data bases
+                res.status(200).send(data)        
          }
          catch(err){
             res.status(500).json({message:"Internal server",error:err})
@@ -85,7 +95,7 @@ app.get("/v2/posts", async (req, res) => {
     
 });
 
-// Fetch the dat fro front end
+// Fetch the data by channel 
 
 app.put("/v3/posts/:channel", async (req, res) => {
     try {
@@ -96,7 +106,8 @@ app.put("/v3/posts/:channel", async (req, res) => {
 
         // Check if any documents were updated
         if (updatedData.matchedCount === 0) {
-            return res.status(404).json({ message: "No data found for this channel" });
+            return res.status(404).json({ message: "No data found for this channel" }); // this if retrun UpdataeData == 0 
+
         }
 
         res.status(200).json({ message: "Data updated successfully", updatedCount: updatedData.modifiedCount });
@@ -108,10 +119,10 @@ app.put("/v3/posts/:channel", async (req, res) => {
 
 
 
-// this for feach new comment 
+// this for feach new comment   by channel 
 app.get("/v4/newpost/:channel", async (req, res)=>{
     try{
-        const data =await Videos.find({channel:req.params.channel})
+        const data =await Videos.find({channel:req.params.channel}) // fetch dat by req.params.channel 
         res.status(200).send(data)
  }
  catch(err){
@@ -122,7 +133,8 @@ app.get("/v4/newpost/:channel", async (req, res)=>{
 
 } )
 
-// listen 
+// listen  to the port of server . js 
+
 
 
 app.listen(Port,()=>console.log(`listening on localhost:${Port}`))
